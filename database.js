b8f363a16d4f396ac8a7f5503a52fddaa1d023cd
Gabriel Hoff <gabriel@i360tecnologia.com.br>
2014-05-25 02:05:43 -0300

    inserção com mongo

--- database.js
+++ database.js
@@ -3,18 +3,27 @@ var config = require("./config.js");
 var mongo = require("mongodb").MongoClient;
 
 module.exports = {
+	_mongoConnection: null,
 	do: function(callback) {
-		//check connection
-		mongo.connect(config.mongo.url, function(err, db) {
-			if (err) {
-				console.log("Erro ao conectar ao servidor do mongo: " + config.mongo.url);
-				console.dir(err);
-				return;
-			}
+		if (!this._mongoConnection) {
+			var self = this;
+			mongo.connect(config.mongo.url, function(err, db) {
+				if (err) {
+					console.log("Erro ao conectar ao servidor do mongo: " + config.mongo.url);
+					console.dir(err);
+					return;
+				}
 
+				self._mongoConnection = db;
+
+				if (typeof(callback) == "function") {
+					callback(err, db);
+				}
+			});
+		} else {
 			if (typeof(callback) == "function") {
-				callback(err, db);
+				callback(null, this._mongoConnection);
 			}
-		});
+		}
 	}
 };
--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,34 @@ router.get('/', function(req, res) {
 	res.render('index', { title: 'Teste' });
 });
 
-router.get("/pudim", function(req, res) {
-	database.do(function(err, db) {
-		var registers = db.collection("registers");
+router.post("/register/add", function(req, res) {
+	//!req.xhr || 
+	if (!req.body || typeof(req.body) != "object") {
+		console.log("pudim");
+		next(); //404 not found
+		return;
+	}
 
-		registers.find().toArray(function(error, result) {
-			res.render("teste", {title: "blabla", registers: result});
+	var register = req.body;
+	console.log(register);
+
+	var mandatoryFields = ["name", "cpf", "bornDate", "rg", "feevaleCode", "course", "email", "phone"];
+	for (var i in mandatoryFields) {
+		var field = mandatoryFields[i];
+
+		if (typeof(register[field]) != "string" || register[field].trim().length == 0) {
+			res.send("200", {success: false, message: field + " is mandatory"});
+			return;
+		}
+	}
+
+	database.do(function(err, db) {
+		db.collection("registers").insert(register, function(err) {
+			if (err) {
+				throw err;
+			}
+
+			res.send("200", {success: true})
 		});
 	});
 });
